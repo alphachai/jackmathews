23637c6650fb70490d81b106adb6c47fa3624231
Charlie <charlie@charliemathews.com>
2019-09-28 13:39:37 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+//particlesJS.load('particles', 'assets/particles.json');
+
+// handle loading animation
+$(document).ready(function($) {
+	$("#preloader-wrap").animate({opacity: 0}, 400);
+	setTimeout(function() {$("#loading").animate({opacity: 0}, 200);}, 400);
+	setTimeout(function() {$("#loading").remove();}, 600);
+});
+
+function showMenu() {
+	$("#burger").addClass("cross");
+	$("#menu").addClass("open");
+	$("#content").css("opacity", "0");
+	$("#title_cursor").css("visibility", "visible");
+}
+
+function hideMenu() {
+	$("#burger").removeClass("cross");
+	$("#menu").removeClass("open");
+	$("#content").css("opacity", "1");
+	$("#title_cursor").css("visibility", "hidden");
+}
+
+// menu animation
+$("#burger").click(function() {
+  if(!$(this).hasClass("cross")) {
+		showMenu();
+	} else {
+		hideMenu();
+	}
+});
+
+// hide menu if user clicks on content? need to improve.
+$("#content").click(function() {
+  if($("#burger").hasClass("cross")) hideMenu();
+});
+
+// hide menu if user scrolls
+$(window).scroll(function() {
+  if($("#burger").hasClass("cross")) hideMenu();
+});
