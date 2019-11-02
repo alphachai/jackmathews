c5a9d01718079196879ef6c4151dc48bf183e14e
Charlie <charlie@charliemathews.com>
2019-11-02 14:03:41 -0500

    Move configuration to config dir

---
+++ config/gunicorn.py
@@ -0,0 +1,30 @@
+# import os
+# from multiprocessing import cpu_count
+#
+# import environ
+# from mintel_logging import LogLevel
+# from mintel_logging.contrib import BaseGunicornLogger
+#
+# is_development = os.path.exists(str((environ.Path(__file__) - 2).path(".env")))
+# env = environ.Env()
+# cpu_reservation = env("DJANGO_CPU", default=512)
+#
+# workers = 2 if is_development else (cpu_reservation // 256) + 1
+# bind = ["0.0.0.0:80"]
+# accesslog = "-"
+# errorlog = "-"
+#
+#
+# class GunicornLogger(BaseGunicornLogger):
+#     SERVICE = "everest.bucket_api"
+#     LEVEL = env(
+#         "MINTEL_LOGGING_LEVEL", default=LogLevel.INFO, cast=LogLevel.__members__.get
+#     )
+#
+#
+# logger_class = GunicornLogger
+#
+# for k, v in os.environ.items():
+#     if k.startswith("GUNICORN_"):
+#         key = k.split("_", 1)[1].lower()
+#         locals()[key] = v
---
+++ config/urls.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+from django.http import HttpResponse
+from django.urls import include, path
+
+urlpatterns = [
+    path("health", HttpResponse("Ok.")),
+    path("ready", include("health_check.urls")),
+    path("admin/", admin.site.urls),
+    path("", include("api.urls")),
+]
+
+if settings.DEBUG:
+    # Add view for Django settings
+    from config import debug
+
+    urlpatterns += [path(r"_debug_info/", debug.splode)]
+
+    # Add views to debug error templates
+    import django.views.defaults
+
+    urlpatterns += [
+        path(r"_debug_404/", django.views.defaults.page_not_found),
+        path(r"_debug_500/", django.views.defaults.server_error),
+        path(r"_debug_400/", django.views.defaults.bad_request),
+        path(r"_debug_403/", django.views.defaults.permission_denied),
+    ]
+
+    # Add static files URL
+    import django.contrib.staticfiles.urls
+
+    urlpatterns += django.contrib.staticfiles.urls.urlpatterns
